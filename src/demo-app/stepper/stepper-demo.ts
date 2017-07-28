3d0f7774b3172bee742cec81a67e1cfde9a2a330
Ji Won Shin <jwshin@google.com>
2017-07-27 20:52:22 -0700

    Add form controls and custom error state matcher for linear stepper

--- src/demo-app/stepper/stepper-demo.ts
+++ src/demo-app/stepper/stepper-demo.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +8,24 @@ import {Component} from '@angular/core';
   styleUrls: ['stepper-demo.scss'],
 })
 export class StepperDemo {
+  nameFormGroup: FormGroup;
+  phoneFormGroup: FormGroup;
+
   steps = [
     {label: 'Confirm your name', content: 'Last name, First name.'},
     {label: 'Confirm your contact information', content: '123-456-7890'},
     {label: 'Confirm your address', content: '1600 Amphitheater Pkwy MTV'},
     {label: 'You are now done', content: 'Finished!'}
   ];
+
+  ngOnInit() {
+    this.nameFormGroup = new FormGroup({
+      firstNameFormCtrl: new FormControl('', Validators.required),
+      lastNameFormCtrl: new FormControl('', Validators.required)
+    });
+
+    this.phoneFormGroup = new FormGroup({
+      phoneFormCtrl: new FormControl('', Validators.required)
+    });
+  }
 }
