de87cc5b396d0e4c8cb82d570a0eaa08c1a0b315
Ji Won Shin <jwshin@google.com>
2017-07-28 08:56:35 -0700

    Add form control - consider each step as its own form group

--- src/demo-app/stepper/stepper-demo.ts
+++ src/demo-app/stepper/stepper-demo.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
